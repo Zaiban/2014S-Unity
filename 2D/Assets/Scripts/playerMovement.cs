2eed8405ef091a76597eaa2c3a60ef0277c8c492
Zaiban <esaparkkila@gmail.com>
2014-11-27 09:42:16 +0200

    2D created

---
+++ 2D/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	public float maxSpeed = 6.0f;
+	public bool facingRight = true;
+	public float moveDirection;
+
+	void FixedUpdate()
+	{
+		rigidbody.velocity = new Vector2 (moveDirection * maxSpeed, rigidbody.velocity.y);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		moveDirection = Input.GetAxis ("Horizontal");
+	}
+}
